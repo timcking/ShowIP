563d376611667c462c45528c9fa12467ed6f0a24
Tim King <timcking@gmail.com>
2013-03-04 17:35:21 -0800

    Minor updates

--- ShowIP.py
+++ ShowIP.py
@@ -28,7 +28,7 @@ class MyApp(wx.App):
 
     def getIP(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("gmail.com",80))
+        s.connect(("gmail.com", 80))
         ip = s.getsockname()[0]
         s.close()
         return ip
